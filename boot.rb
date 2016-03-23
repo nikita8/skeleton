62d9d2e8cf02067314f2e47f792a365c978c9cc0
Nikita Acharya <niks_ach@yahoo.com>
2016-03-23 09:16:26 +0545

    Tell grape to use database configuration from yaml file.

--- boot.rb
+++ boot.rb
@@ -1,4 +1,14 @@
 require 'rubygems'
 require 'bundler'
 require 'digest/sha1'
-require File.expand_path('../config/environment.rb', __FILE__)
+
+module App
+  def self.root
+    @root_path ||= FileUtils.pwd
+  end
+end
+
+require File.expand_path("#{App.root}/config/environment.rb", __FILE__)
+
+Grape::ActiveRecord.configure_from_file! "config/database.yml"
+ActiveRecord::Base.logger = AppLogger.logger
